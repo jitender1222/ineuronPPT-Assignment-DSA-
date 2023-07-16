3cd94128ad34340d9a6d6f03bb2ac07291fffbbd
jitender singh <jeetusinghkanyal@gmail.com>
2023-07-16 22:32:08 +0530

    other method

--- Arrays/Assignment-6 2DArray/Q5.cpp
+++ Arrays/Assignment-6 2DArray/Q5.cpp
@@ -1,3 +1,38 @@
+// #include <iostream>
+// #include <vector>
+// #include <algorithm>
+
+// using namespace std;
+
+// int minProductSum(vector<int> &nums1, vector<int> &nums2)
+// {
+//     sort(nums1.begin(), nums1.end());
+//     sort(nums2.rbegin(), nums2.rend());
+
+//     int productSum = 0;
+
+//     for (int i = 0; i < nums1.size(); i++)
+//     {
+//         productSum += nums1[i] * nums2[i];
+//     }
+
+//     return productSum;
+// }
+
+// int main()
+// {
+//     vector<int> nums1 = {5, 3, 4, 2};
+//     vector<int> nums2 = {4, 2, 2, 5};
+
+//     int result = minProductSum(nums1, nums2);
+
+//     cout << result << endl;
+
+//     return 0;
+// }
+
+// diff method without sorting array
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -6,14 +41,15 @@ using namespace std;
 
 int minProductSum(vector<int> &nums1, vector<int> &nums2)
 {
-    sort(nums1.begin(), nums1.end());
-    sort(nums2.rbegin(), nums2.rend());
-
     int productSum = 0;
+    int n = nums1.size();
 
-    for (int i = 0; i < nums1.size(); i++)
+    sort(nums1.begin(), nums1.end());
+    sort(nums2.begin(), nums2.end(), greater<int>());
+
+    for (int i = 0; i < n; i++)
     {
-        productSum += nums1[i] * nums2[i];
+        productSum += nums1[i] * nums2[n - i - 1];
     }
 
     return productSum;
